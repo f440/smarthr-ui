e8bf189d5a7f7af498e52a41db24f62d6768340b
f440 <freq440@gmail.com>
2023-05-20 13:19:26 +0900

    chore: Enable consistent-type-exports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,17 @@
 module.exports = {
   extends: ['smarthr', 'plugin:storybook/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
   rules: {
     'jsx-a11y/anchor-is-valid': 'warn',
     'jsx-a11y/click-events-have-key-events': 'warn',
     'jsx-a11y/no-static-element-interactions': 'warn',
     'smarthr/a11y-anchor-has-href-attribute': 'error',
     'smarthr/a11y-prohibit-input-placeholder': 'error',
+    '@typescript-eslint/consistent-type-exports': 'error',
   },
 }
