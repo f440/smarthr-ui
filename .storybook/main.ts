4684f8153c6b2da3e2e0938c83884e1c6df7bce3
f440 <freq440@gmail.com>
2024-03-12 15:24:48 +0900

    feat: Remove unused settings

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -32,11 +32,6 @@ const config: StorybookConfig = {
           {
             test: /\.css$/,
             use: [
-              'style-loader',
-              {
-                loader: 'css-loader',
-                options: { importLoaders: 1 },
-              },
               {
                 // Gets options from `postcss.config.js` in your project root
                 loader: 'postcss-loader',
